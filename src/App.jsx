31eb9602441a2f47651be24e2cc013ad6b2e3bb6
Kritikas8349 <kritikasingh8349@gmail.com>
2025-12-24 14:50:03 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Menu from './components/Menu';   
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <>
+      <ScrollToTop/>
       <Navbar />
 
       <Routes>
